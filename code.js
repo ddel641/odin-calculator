2a926ae0d97bbfa07e6aa6ec93a86d2f4785f61f
ddel641 <daniel.delpiano@gmail.com>
2021-12-07 17:22:12 -0500

    Add code to make first operation work

--- code.js
+++ code.js
@@ -1,73 +1,53 @@
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display");
 
-let firstNumber = "0";
-let secondNumber = "0";
-let answer = null;
-let operation = null;
+let operation = {
+    firstNumber: "0",
+    secondNumber: "0",
+    answer: null,
+    sign: null,
+    decimal: false,
 
-
+    reset: function () {
+        this.firstNumber = "0";
+        this.secondNumber = "0";
+        this.answer = null; 
+        this.sign = null;
+        this.decimal = false;
+    },
+};
 
 function updateDisplay(value) {
     display.innerHTML = value;
-
-    // if (!operation && !answer) {
-    //     console.log(value);
-    //     display.innerHTML += value;
-    //     firstNumber = display.innerHTML;
-    // } else {
-    //     display.innerHTML = secondNumber;
-    //     display.innerHTML += value;
-    //     secondNumber = display.innerHTML;
-    // }
-    // console.log(firstNumber, secondNumber);
 }
 
 function processKeyValue(value) {
+    let working = (!operation["sign"]) ? "firstNumber" : "secondNumber";
+
     if (value.match(/[0-9]/)) {
-        if (firstNumber === "0") {
-            firstNumber = value;
-        } else value : firstNumber += value;
-        updateDisplay(value);
+        if (operation[working] === "0") {
+            operation[working] = value;
+        } else {
+            operation[working] += value;
+        }
+    } else if (value === "." && !operation["decimal"]) {
+        operation[working] += value;
+        operation["decimal"] = true;
+    } else if (value.match(/[\+\-\*\/]/)) {
+        if (!operation["sign"]) {
+            operation["sign"] = value;
+            operation["decimal"] = false;
+        } 
+    } else if (value === "=") {
+        working = "answer";
+        operation[working] = operate(operation["firstNumber"],
+                                      operation["secondNumber"],
+                                      operation["sign"]);
+        operation.firstNumber = operation.answer;
+    } else if (value === "Clear") {
+        operation.reset();
     }
-
-    // if (value.match(/[0-9]/)) {
-    //     if (!operation)
-    //      if (this.innerHTML === "." && !decimal) {
-    //          decimal = true;
-    //      } else if (this.innerHTML === "." && decimal) {
-    //          return
-    //      }
-    //      updateDisplay(this.innerHTML);
-         
-    //  } else if (this.innerHTML.match(/[\+\-\*\/]/)) {
-    //      operation = this.innerHTML;
-    //      decimal = false;
-    //      console.log(operation);
-    //  } else if (this.innerHTML === "=") {
-    //      doMath();
-    //  } else if (this.innerHTML === "Clear") {
-    //      resetCalculator();
-    //  }
-
-}
-
-
-buttons.forEach((button) => {
-    
-    button.addEventListener("click", function (e) {
-        processKeyValue(this.innerHTML)
-    });
-});
-
-
-function resetCalculator () {
-    firstNumber = "";
-    secondNumber = "";
-    operation = "";
-    answer = "";
-    display.innerHTML = "0";
-    decimal = false;
+    updateDisplay(operation[working]);
 }
 
 function add(a, b) {
@@ -87,21 +67,29 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
+    [a, b] = [Number(a), Number(b)];
     let answer;
     switch (operator) {
         case "+":
             answer = add(a, b);
             break;
-        case "-": 
+            case "-": 
             answer = subtract(a, b);
             break;
-        case "/": 
+            case "/": 
             answer = divide(a, b);
             break;
-        case "*":
-            answer = multiply(a, b);
-        default: 
-            break;
-    }
-    return answer;
-}
+            case "*":
+                answer = multiply(a, b);
+                default: 
+                break;
+            }
+            return answer
+        }
+
+buttons.forEach((button) => {
+            
+    button.addEventListener("click", function (e) {
+        processKeyValue(this.innerHTML);
+    });
+});
